bf45bd364c681a334dfd210259f4eccb8f6c6cd8
Nikolai Merritt <nikolaimerritt20@gmail.com>
2022-03-02 11:38:32 +0000

    can query for stations along line

--- populate_database.py
+++ populate_database.py
@@ -1,2 +1,36 @@
 import query_database
 import json
+
+
+def readJson():
+    filename = "train-network.json"
+    with open(filename, "r") as file:
+        return json.load(file)
+
+
+def populateStations(stations):
+    for station in stations:
+        id = station["id"]
+        name = station["name"]
+        latitude = float(station["latitude"])
+        longitude = float(station["longitude"])
+        query_database.createStation(id, name, latitude, longitude)
+
+
+
+def populateLines(lines):
+    for line in lines:
+        lineName = line["name"]
+        query_database.createLine(lineName)
+        for stationId in line["stations"]:
+            query_database.createConnection(stationId, lineName)
+
+
+
+jsonRead = readJson()
+stations = jsonRead["stations"]
+lines = jsonRead["lines"]
+
+query_database.createLine("Circle")
+populateStations(stations)
+populateLines(lines)
--- query_database.py
+++ query_database.py
@@ -12,9 +12,9 @@ connection = mysql.connector.connect(user="root", password=readPassword(), datab
 def createLine(lineName):
     cursor = connection.cursor()
     query = f"INSERT IGNORE INTO Line (line_name) " + \
-        f"VALUES ('{lineName}')"
+        f"VALUES (%s)"
 
-    cursor.execute(query)
+    cursor.execute(query, (lineName, ))
     connection.commit()
     cursor.close()
 
@@ -22,9 +22,9 @@ def createLine(lineName):
 def createStation(stationId, name, latitude, longitude):
     cursor = connection.cursor()
     query = f"INSERT IGNORE INTO Station (station_id, station_name, latitude, longitude) " + \
-        f"VALUES ('{stationId}', '{name}', {latitude}, {longitude})"
+        f"VALUES (%s, %s, %s, %s)"
     
-    cursor.execute(query)
+    cursor.execute(query, (stationId, name, latitude, longitude))
     connection.commit()
     cursor.close()
 
@@ -32,13 +32,27 @@ def createStation(stationId, name, latitude, longitude):
 def createConnection(stationId, lineName):
     cursor = connection.cursor()
     query = f"INSERT IGNORE INTO Connection (station_id, line_name) " + \
-        f"VALUES ('{stationId}', '{lineName}')"
+        f"VALUES (%s, %s)"
     
-    cursor.execute(query)
+    cursor.execute(query, (stationId, lineName))
     connection.commit()
     cursor.close()
 
 
-# createLine(lineName="District")
+def readStationsAlongLine(lineName):
+    cursor = connection.cursor()
+    stationNameQuery = f"SELECT Station.station_name FROM Station " + \
+        f"INNER JOIN Connection ON (Connection.station_id = Station.station_id)" + \
+        f"WHERE (Connection.line_name = %s)"
+    
+    cursor.execute(stationNameQuery, (lineName, ))
+    names = []
+    for (name, ) in cursor:
+        names.append(name)
+    
+    return names
+
+
+readStationsAlongLine("District")
 # createStation(stationId="940GZZLUTWH", name="Tower Hill", latitude=51.509971, longitude=-0.076546)
 # createConnection(stationId="940GZZLUTWH", lineName="District")
